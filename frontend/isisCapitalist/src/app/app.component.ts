f3c5c1a2cf9220c1768bc65a921f4548c1c07698
loisb <lois.garcion34@gmail.com>
2024-01-24 14:19:46 +0100

    J'ai commencé à faire la gestion utilisateur coté front, maintenant coté back

--- frontend/isisCapitalist/src/app/app.component.ts
+++ frontend/isisCapitalist/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { World, Palier, Product } from './world';
 import { WebserviceService } from './webservice.service';
+import { FormsModule } from '@angular/forms';
 
 export const GET_SERV = "http://localhost:4000/";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,6 +17,16 @@ export class AppComponent {
   title = 'isisCapitalist';
   server = GET_SERV;
   world: World = new World();
+  username: string = localStorage.getItem('username')?.toString() ?? Math.floor(Math.random()*100000).toString();
+
+  onUsernameChanged(){
+    if(this.username == ""){
+      this.username = Math.floor(Math.random()*100000).toString();
+    }
+    localStorage.setItem('username', this.username);
+    console.log("test")
+  }
+
   constructor(private service: WebserviceService) {
     this.service.getWorld().then(
       world => {
