2c7d485a1c21927783dc2f1cd0ccd1de22fc2bc8
LoisGarcion <lois.garcion34@gmail.com>
2024-03-08 17:59:48 +0100

    Progress bar commencée, maintenant faut finaliser

--- frontend/isisCapitalist/src/app/product/product.component.ts
+++ frontend/isisCapitalist/src/app/product/product.component.ts
@@ -1,20 +1,43 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {Component, Inject, Input, OnChanges, PLATFORM_ID, signal} from '@angular/core';
 import { Product } from '../world';
 import {GET_SERV} from "../app.component";
 import {BigvaluePipe} from "../bigvalue.pipe";
+import {MatProgressBarModule} from '@angular/material/progress-bar'
+import {isPlatformBrowser} from "@angular/common";
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [
-    BigvaluePipe
+    BigvaluePipe,
+    MatProgressBarModule
   ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent implements OnChanges {
   product: Product = new Product();
-  constructor() {
+  progressbarvalue: number = 0;
+  isBrowser = signal(false);
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+  ) {
+    this.isBrowser.set(isPlatformBrowser(platformId));
+  }
+
+  ngAfterViewInit() {
+    if (this.isBrowser()) {
+      setInterval(() => { this.calcScore() }, 100)
+    }
+  }
+
+  calcScore() {
+    //TODO ICI
+  }
+
+  startProduction() {
+
   }
 
   @Input()
